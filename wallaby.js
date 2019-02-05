d9c728f31736fbfe0a1938371527a8c7a6781701
Alex Novikov <7309407@gmail.com>
2019-02-05 20:21:17 -0300

    setup unit test environment;

---
+++ wallaby.js
@@ -0,0 +1,19 @@
+module.exports = function () {
+    return {
+        files: [
+            'src/**/*.ts',
+            '!src/**/*.spec.ts',
+        ],
+
+        tests: [
+            'src/**/*.spec.ts'
+        ],
+
+        env: {
+            type: 'node'
+        },
+
+        testFramework: 'jasmine',
+
+    };
+};
